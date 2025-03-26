e1b76c38d9ea1bf579ccabb96d150ecebd9dd4d3
Damilola Joshua <damilolajoshua2021@gmail.com>
2025-03-26 13:15:52 -0700

    added hover pseudo-classes

---
+++ scripts.js
@@ -0,0 +1,10 @@
+// console.log('Hello, World!');
+
+// const lmao = "Wha the Hell0";
+// console.log(lmao);
+
+
+document.querySelector('.js-home-button').
+  addEventListener('click', () => {
+    document.querySelector('.js-home-screen');
+  });
