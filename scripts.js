7934b4b85dbdcd37681b5b7b2bcfb0b6d27fd8d5
Damilola Joshua <damilolajoshua2021@gmail.com>
2025-03-26 18:29:37 -0700

    Added Javascript

--- scripts.js
+++ scripts.js
@@ -4,7 +4,33 @@
 // console.log(lmao);
 
 
-document.querySelector('.js-home-button').
-  addEventListener('click', () => {
-    document.querySelector('.js-home-screen');
+document.addEventListener('DOMContentLoaded', () => {
+  //Mapping Links to their section
+  const scrollLinks = {
+    "homeLink": "home",
+    "aboutUsLink" : "about",
+    "ourServicesLink": "services",
+    "contactUsLink": "contact"
+  }
+
+  Object.keys(scrollLinks).forEach((linkId) => {
+    console.log(linkId);
+    const linkElement = document
+      .getElementById(linkId);
+    console.log(linkElement);
+
+    const targetSection = document
+      .getElementById(scrollLinks[linkId]);
+    
+    console.log(targetSection);
+
+    if (scrollLinks && targetSection) {
+      linkElement.addEventListener('click', () => {
+        targetSection.scrollIntoView({
+          behavior: "smooth"
+        });
+      });
+    }
   });
+
+});
