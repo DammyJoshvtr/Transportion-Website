b00682411f8ec0efd181c982105c5f0db7bfbd5c
Damilola Joshua <damilolajoshua2021@gmail.com>
2025-03-27 03:31:21 -0700

    Worked on the Nav with JS

--- scripts.js
+++ scripts.js
@@ -4,33 +4,75 @@
 // console.log(lmao);
 
 
-document.addEventListener('DOMContentLoaded', () => {
-  //Mapping Links to their section
-  const scrollLinks = {
-    "homeLink": "home",
-    "aboutUsLink" : "about",
-    "ourServicesLink": "services",
-    "contactUsLink": "contact"
-  }
+// document.addEventListener('DOMContentLoaded', () => {
+//   //Mapping Links to their section
+//   const scrollLinks = {
+//     "homeLink": "home",
+//     "aboutUsLink" : "about",
+//     "ourServicesLink": "services",
+//     "contactUsLink": "contact"
+//   }
 
-  Object.keys(scrollLinks).forEach((linkId) => {
-    console.log(linkId);
-    const linkElement = document
-      .getElementById(linkId);
-    console.log(linkElement);
+//   Object.keys(scrollLinks).forEach((linkId) => {
+//     console.log(linkId);
+//     const linkElement = document
+//       .getElementById(linkId);
+//     console.log(linkElement);
 
-    const targetSection = document
-      .getElementById(scrollLinks[linkId]);
+//     const targetSection = document
+//       .getElementById(scrollLinks[linkId]);
     
-    console.log(targetSection);
+//     console.log(targetSection);
 
-    if (scrollLinks && targetSection) {
-      linkElement.addEventListener('click', () => {
-        targetSection.scrollIntoView({
-          behavior: "smooth"
-        });
-      });
-    }
-  });
+//     if (linkElement && targetSection) {
+//       linkElement.addEventListener('click', () => {
+//         targetSection.scrollIntoView({
+//           behavior: "smooth"
+//         });
+//       });
+//     }
+//   });
 
-});
+// });
+
+
+//The code Below Works
+
+// const buttonElement = document.getElementById('ourServicesLink');
+// console.log(buttonElement);
+
+// buttonElement.addEventListener('click', () => {
+//   const targetElement = document.getElementById('services');
+
+//   targetElement.scrollIntoView({
+//     behavior: "smooth"
+//   });
+// });
+
+
+
+//Navigation Codes
+const navButtons = {
+  "homeLink": "home",
+  "aboutUsLink": "about",
+  "ourServicesLink": "services",
+  "contactUsLink": "contact"  
+}
+
+console.log(Object.keys(navButtons));
+
+Object.keys(navButtons).forEach((key) => {
+  console.log(key, 'this is the are object keys');
+
+  const buttonElement = document.getElementById(key);
+  console.log(buttonElement);
+
+  const targetElement = document.getElementById(navButtons[key]);
+  console.log(targetElement);
+
+  buttonElement.addEventListener('click', () => {
+    targetElement.scrollIntoView({
+      behavior: "smooth"
+    })
+  })
+}) 
